0e67973a90ad3072601663f856f6c1127b0e6111
leolaureys <leo.laureys@ind-philippeville.be>
2026-03-24 12:59:16 +0100

    ok

--- Gestion_Salle_de_Cinema/Fonctions_GSC.py
+++ Gestion_Salle_de_Cinema/Fonctions_GSC.py
@@ -1,3 +1,5 @@
+
+
 def print_tableaux(tableaux):
     for ligne in tableaux:
             affichage = "" 
@@ -6,9 +8,24 @@ def print_tableaux(tableaux):
             print(affichage)
 
 def create_tableaux(tableaux, nbr_ligne, nbr_colone):
+    
+    nbr_ligne += 1
+    nbr_colone += 1
+
     for i in range(nbr_ligne):
         ligne = []
-        for i in range(nbr_colone):
-            ligne.append("□")#■
-        tableaux.append(ligne)
-    i
+        if i == 0:
+            for j in range(nbr_colone):
+                ligne.append(f"{j}")
+            tableaux.append(ligne)
+        else:
+            for j in range(nbr_colone):
+                if j == 0:
+                    lettre = chr(64 + i)
+                    ligne.append(f"{lettre}")
+                else:
+                    ligne.append("□")#■
+            tableaux.append(ligne)
+    tableaux[0][0] = " "
+    
+    
