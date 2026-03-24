41ef77b52148c3fab537898419838378e7aec9fa
leolaureys <leo.laureys@ind-philippeville.be>
2026-03-24 09:00:36 +0100

    ok

--- Gestion_Salle_de_Cinema/Main_GSC.py
+++ Gestion_Salle_de_Cinema/Main_GSC.py
@@ -1,1 +1,18 @@
 import Fonctions_GSC as fg
+
+salle_cinema = []
+
+logueur_ligne = 3
+logueur_colone = 5
+
+for i in range(1,0,logueur_ligne):
+    ligne = []
+    for j in range(1,0,logueur_colone):
+        ligne.append("#")
+        salle_cinema.append(ligne)
+
+for i in salle_cinema:
+            to_display = "" 
+            for casse in i:
+                to_display += casse + " "
+            print(to_display)   
