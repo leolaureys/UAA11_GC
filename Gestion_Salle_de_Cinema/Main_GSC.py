8239c337e5034296df0c03b7cab7a399e60b632e
leolaureys <leo.laureys@ind-philippeville.be>
2026-03-26 10:10:45 +0100

    ok

--- Gestion_Salle_de_Cinema/Main_GSC.py
+++ Gestion_Salle_de_Cinema/Main_GSC.py
@@ -5,5 +5,16 @@ salle_cinema = []
 nbr_ligne = 0                                                                       
 nbr_colone = 0
 
+continuer_code = True
 
+choix = 1
 
+while continuer_code == True:
+    try:
+
+        F_GSC.print_menu()
+
+        choix = input("=====> ")
+
+    except(ValueError):
+        print("\n\nEreur de Valeur !")
