97413faf631360ae5e78b71c1186b58990f19599
leolaureys <leo.laureys@ind-philippeville.be>
2026-03-26 14:24:41 +0100

    ok

--- Gestion_Salle_de_Cinema/Main_GSC.py
+++ Gestion_Salle_de_Cinema/Main_GSC.py
@@ -9,12 +9,27 @@ continuer_code = True
 
 choix = 1
 
+F_GSC.create_tableaux(salle_cinema, nbr_ligne, nbr_colone)
+
 while continuer_code == True:
-    try:
 
-        F_GSC.print_menu()
+    F_GSC.print_menu()
+    choix = int(input("\n=====> "))
 
-        choix = input("=====> ")
-
-    except(ValueError):
-        print("\n\nEreur de Valeur !")
+    if choix == 1:
+        F_GSC.print_tableaux(salle_cinema)
+    elif choix == 2:
+        F_GSC.reserver_place(salle_cinema)
+    elif choix == 3:
+        F_GSC.delete_reservation(salle_cinema)
+    elif choix == 4:
+        F_GSC.place_disponible(salle_cinema)
+    elif choix == 5:
+        F_GSC.compter_place(salle_cinema)
+    elif choix == 6:
+        F_GSC.create_tableaux(salle_cinema, nbr_ligne, nbr_colone)
+    elif choix == 7:
+        continuer_code = False
+        print("Au revoir !")
+    else:
+        print("Choix non-valide !")
